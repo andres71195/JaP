584a939b7060e22b6483c884d9e00234cb0de7de
andres71195 <asuarez71195@gmail.com>
2022-09-18 17:41:25 -0300

    8
    Modificación botón google.

--- js/login.js
+++ js/login.js
@@ -4,27 +4,31 @@ const ingresar = document.getElementById("ingresar");
 const alertEmail = document.getElementById("alertEmail")
 
 ingresar.addEventListener("click", (e) => {
-    if (email.value === '') 
-    { e.preventDefault();
-    document.getElementById("errorEmail").classList.remove("d-none");
+    if (email.value === '') {
+        e.preventDefault();
+        document.getElementById("errorEmail").classList.remove("d-none");
     }
     if (contraseña.value === '') {
         e.preventDefault();
-    document.getElementById("errorContra").classList.remove("d-none")
+        document.getElementById("errorContra").classList.remove("d-none")
     }
 
     else {
-    window.location.href='main.html'
+        window.location.href = 'main.html'
     };
 
     if (email.value) {
         localStorage.setItem("usuario", email.value)
     };
 
-})
+});
 
-// ingresar.addEventListener("click", (e) =>{
-// if (email.value) {
-//     localStorage.setItem("usuario", email.value)
-// }
-// })
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: '595523676664-9vdo96luuflek3jkoqtt46tv1ehuirm0.apps.googleusercontent.com',
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.prompt();
+};
+
+window.onload();
